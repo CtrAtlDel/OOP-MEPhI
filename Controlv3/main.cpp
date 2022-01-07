b611195e4b1315d22085e8b6ee67d4b5050a346c
CtrAtlDelete <kvasovski34@gmail.com>
2022-01-07 14:01:53 +0300

    Vector template done.

--- Controlv3/main.cpp
+++ Controlv3/main.cpp
@@ -3,14 +3,14 @@
 #include "Junior.h"
 
 int main() {
-    Console::Junior jun;
-    int size = 10;
-    for (int i = 0; i < size; ++i) {
-        jun.setRating(i);
-    }
-    templates::vector<double> tmp = jun.getRating();
-    for (auto &it: tmp) {
-        std::cout << it << std::endl;
-    }
+//    Console::Junior jun;
+//    int size = 10;
+//    for (int i = 0; i < size; ++i) {
+//        jun.setRating(i);
+//    }
+//    templates::vector<double> tmp = jun.getRating();
+//    for (auto &it: tmp) {
+//        std::cout << it << std::endl;
+//    }
     return 0;
 }
